d53a066402edd0a07852459227c6ee5507bbbaa9
julianof03 <jlprod08@gmail.com>
2022-08-31 10:15:23 -0300

    BaseScript

---
+++ index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const users = [];
+const messages = [];
+
+app.post('/participants', (req, res) => {
+    const userName = req.body;
+    users.push(userName);
+    res.sendStatus(202);
+});
+
+app.listen(5000), () => console.log("Listening on port 5000");
