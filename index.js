11ba8f5cb14d9bceb7fe85fd4d984672de73f812
julianof03 <jlprod08@gmail.com>
2022-09-02 09:19:21 -0300

    Get/Post Participants

--- index.js
+++ index.js
@@ -1,16 +1,47 @@
 import express from "express";
 import cors from "cors";
+import { MongoClient } from "mongodb";
+import dayjs from "dayjs";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-const users = [];
-app.post('/participants', (req, res) => {
-    const userName = req.body;
-    users.push(userName);
-    res.sendStatus(202);
+
+const mongoClient = new MongoClient(process.env.MONGO_URI);
+let db;
+mongoClient.connect().then(() => {
+	db = mongoClient.db("UOLdb");
 });
+
+const users = [];
+app.post('/participants', async (req, res) => {
+    try{
+        const {name} = req.body;
+        db.collection("participants").insertOne({
+            name,
+            lastStatus: Date.now(),
+        });
+        
+        
+        res.sendStatus(201);
+    } catch {
+        res.sendStatus(500);
+    }
+    
+});
+app.get('/participants', (req, res) => {
+    try{
+        db.collection("participants").find().toArray().then(users => {
+            console.log(users); 
+        });  
+        res.sendStatus(201);
+    } catch {
+        res.sendStatus(500);
+    }
+})
 
 app.listen(5000), () => console.log("Listening on port 5000");
