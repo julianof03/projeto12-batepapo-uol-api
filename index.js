ffe761d87f6a98dbbd1476dfe3e2f081a6f1e814
julianof03 <jlprod08@gmail.com>
2022-09-02 11:16:18 -0300

    Add DeleteUser System

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import { MongoClient } from "mongodb";
 import dayjs from "dayjs";
+import joi from 'joi';
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -17,7 +18,6 @@ mongoClient.connect().then(() => {
 	db = mongoClient.db("UOLdb");
 });
 
-const users = [];
 app.post('/participants', async (req, res) => {
     try{
         const {name} = req.body;
@@ -33,15 +33,23 @@ app.post('/participants', async (req, res) => {
     }
     
 });
-app.get('/participants', (req, res) => {
+app.get('/participants', async (req, res) => {
     try{
         db.collection("participants").find().toArray().then(users => {
-            console.log(users); 
         });  
         res.sendStatus(201);
     } catch {
         res.sendStatus(500);
     }
 })
+
+async function checkUserTime(){
+    const LastTime = Date.now() - 10000;
+    await mongoClient.connect();
+    const awayUser = await db.collection("participants").find({lastStatus: { $lt: LastTime}}).toArray();
+    await db.collection("participants").deleteOne({ id: awayUser.id });
+}
+
+setInterval(checkUserTime, 10000);
 
 app.listen(5000), () => console.log("Listening on port 5000");
