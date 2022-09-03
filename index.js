631a2e56621e0f16be76eb1fed236445deb4878e
julianof03 <jlprod08@gmail.com>
2022-09-03 14:04:41 -0300

    PostStatus and messages

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ import express from "express";
 import cors from "cors";
 import { MongoClient } from "mongodb";
 import dayjs from "dayjs";
-import joi from 'joi';
 import dotenv from "dotenv";
+import Joi from "joi";
 dotenv.config();
 
 const app = express();
@@ -11,43 +11,121 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-
-const mongoClient = new MongoClient(process.env.MONGO_URI);
 let db;
-mongoClient.connect().then(() => {
-	db = mongoClient.db("UOLdb");
-});
+const mongoClient = new MongoClient(process.env.MONGO_URI);
+mongoClient.connect().then(() => { db = mongoClient.db("UOLdb");});
+
 
 app.post('/participants', async (req, res) => {
-    try{
-        const {name} = req.body;
+    const { name } = req.body;
+
+    const nameSchema = Joi.object({ name: Joi.string().required(),});
+    const validateName = nameSchema.validate(req.body);
+
+    if (validateName.error) {
+        res.sendStatus(422);
+    }
+    const OnlineUser = await db.collection("participants").findOne({name});
+    if(OnlineUser){
+        res.status(409).send("Usuario ja existente")    
+    }
+    try {    
         db.collection("participants").insertOne({
             name,
             lastStatus: Date.now(),
         });
-        
-        
+        db.collection("messages").insertOne(
+        {
+            from: name,
+            to: 'Todos',
+            text: 'entra na sala...',
+            type: 'status',
+            time: dayjs().format("HH:mm:ss")
+        }
+        );
         res.sendStatus(201);
     } catch {
         res.sendStatus(500);
     }
-    
+
 });
 app.get('/participants', async (req, res) => {
-    try{
-        db.collection("participants").find().toArray().then(users => {
-        });  
+    try {
+        const participantes = await db.collection("participants").find({}).toArray();
         res.sendStatus(201);
     } catch {
         res.sendStatus(500);
     }
+});
+app.post('/messages', async (req, res) => {
+    const { to, text, type } = req.body;
+    const { from } = req.headers.user;
+    const messageSchema = Joi.object({
+        from,
+        to,
+        text,
+        type,
+        time: dayjs().format("HH:mm:ss")
+    });
+    try {
+        await db.collection("messages").insertOne(
+            {
+                from,
+                to,
+                text,
+                type,
+                time: dayjs().format("HH:mm:ss")
+            }
+        );
+        res.sendStatus(201);
+    } catch {
+        res.sendStatus(500);
+    }
+});
+
+app.get('/messages', async (req, res) => {
+    const { user } = req.headers;
+    await db.collection("messages").deleteMany({ from: null })
+    const list = await db.collection("messages").find().toArray();
+    console.log(list);
+    res.sendStatus(201);
+});
+app.post('/status', async (req, res) => {
+    const {user} = req.headers;
+    const newStatus = await db.collection("participants").findOne({name: user});
+    if(newStatus){
+        await db.collection('participants').updateOne({
+            lastStatus: newStatus.lastStatus,
+        },
+        {
+            $set: {lastStatus: Date.now()}
+        });
+        res.sendStatus(201);
+    } 
+    res.sendStatus(404);
 })
 
-async function checkUserTime(){
+async function checkUserTime() {
     const LastTime = Date.now() - 10000;
     await mongoClient.connect();
-    const awayUser = await db.collection("participants").find({lastStatus: { $lt: LastTime}}).toArray();
-    await db.collection("participants").deleteOne({ id: awayUser.id });
+    const awayUser = await db.collection("participants").find({ lastStatus: { $lt: LastTime } }).toArray();
+
+    console.log(awayUser.name);
+    // if(awayUser != null){
+    //      await db.collection("messages").insertOne(
+    //          {
+    //              from: awayUser.name,
+    //              to,
+    //             text,
+    //             type,
+    //             time: dayjs().format("HH:mm:ss")
+    //          }
+    //      );
+    //     console.log("mandei a menssagem");        
+    //     await db.collection("participants").deleteOne({ id: awayUser.id });
+    //     return
+    // }
+    // console.log("ele ta na sala ainda");
 }
 
 setInterval(checkUserTime, 10000);
